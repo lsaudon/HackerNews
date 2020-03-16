ccf06ad6e338e5eeb2d813b27bb6d4b168b16cba
lsaudon <lsaudon@gmail.com>
2020-03-16 23:01:21 +0100

    Add icon launcher and splash screen on android

--- HackerNews.MobileApp/HackerNews.MobileApp.Android/MainActivity.cs
+++ HackerNews.MobileApp/HackerNews.MobileApp.Android/MainActivity.cs
@@ -2,14 +2,23 @@
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Views;
 
 namespace HackerNews.MobileApp.Droid
 {
-    [Activity(Label = "HackerNews.MobileApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HackerNews.MobileApp",
+        Icon = "@mipmap/icon", 
+        RoundIcon ="@mipmap/icon_round" ,
+        Theme = "@style/MainTheme.Splash", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            SetTheme(Resource.Style.MainTheme);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
